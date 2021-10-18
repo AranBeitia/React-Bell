43232ab3121a11dce0843bee94dfd2bac5f6ab89
AranBeitia <arantza.beitia@gmail.com>
2021-10-18 15:04:23 +0200

    menu

--- src/components/TheMenu/Panel/Panel.js
+++ src/components/TheMenu/Panel/Panel.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 // import PanelStyled from './Panel.style'
 import './Panel.scss'
 
 function Panel({ title, id, path }) {
-	// const [isOpen, SetIsOpen] = useState(false)
+	const [isOpen, SetIsOpen] = useState(false)
 
-	// openPanel = () => {
-	// 	SetIsOpen(!isOpen)
-	// }
+	const openPanel = () => {
+		SetIsOpen(!isOpen)
+	}
 
 	return (
-		<li className={`panel panel--img-${id}`}>
+		<li
+			className={`panel panel--img-${id} ${isOpen ? '--open' : ''}`}
+			onClick={openPanel}
+		>
+			<p>Helmets</p>
+			<p>{title}</p>
 			<NavLink to={path}>{title}</NavLink>
 		</li>
 	)
