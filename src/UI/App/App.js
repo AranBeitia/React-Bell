557351ec9a75392356d13927637522fb5bdb3196
Yaiser <yaiserar@gmail.com>
2021-10-19 18:15:09 +0200

    implement useReducer

--- src/UI/App/App.js
+++ src/UI/App/App.js
@@ -1,114 +1,190 @@
-import React, { useState, useEffect } from 'react'
-import { getProducts } from '../../api'
-import { readLocalStorage, writeLocalStorage } from '../../utils/localstorage'
-import { Switch, Route } from 'react-router-dom'
-import * as routes from '../../constants/routes'
-import ProductContext from '../../context/ProductContext'
-import AuthContext from '../../context/AuthContext'
-import { darkTheme, lightTheme } from '../components/Theme/Theme'
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyles } from '../components/Theme/Global.styles'
+import React, { useEffect, useReducer } from "react";
+import AuthContext from "../../context/AuthContext";
+import ProductContext from "../../context/ProductContext";
+import { darkTheme, lightTheme } from "../components/Theme/Theme";
+import { GlobalStyles } from "../components/Theme/Global.styles";
+import { ThemeProvider } from "styled-components";
+import { getProducts } from "../../api";
+import { readLocalStorage, writeLocalStorage } from "../../utils/localstorage";
+import { Switch, Route } from "react-router-dom";
+import * as routes from "../../constants/routes";
 
-import Home from '../pages/HomePage'
-import Helmets from '../pages/HelmetPage'
-import Login from '../pages/LoginPage'
+import Home from "../pages/HomePage";
+import Helmets from "../pages/HelmetPage";
+import Login from "../pages/LoginPage";
+
+const initialState = {
+  categories: [],
+  isAuth: false,
+  user: {},
+  isLoading: false,
+  theme: "",
+};
+
+const actionTypes = {
+  FETCH_SUCCESS: "FETCH_SUCCESS",
+  FETCH_ERROR: "FETCH_ERROR",
+  CATEGORIES: "CATEGORIES",
+  USER: "USER",
+  AUTH: "AUTH",
+  THEME: "THEME",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_SUCCESS: {
+      return {
+        ...state,
+        isLoading: true,
+        categories: [...action.payload],
+      };
+    }
+    case actionTypes.FETCH_ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+      };
+    }
+    case actionTypes.CATEGORIES: {
+      return {
+        ...state,
+        categories: [...action.payload],
+      };
+    }
+    case actionTypes.USER: {
+      return {
+        ...state,
+        user: { ...action.payload },
+      };
+    }
+    case actionTypes.AUTH: {
+      return {
+        ...state,
+        isAuth: action.payload,
+      };
+    }
+
+    case actionTypes.THEME: {
+      return {
+        ...state,
+        theme: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 function App() {
-	const [categories, setCategories] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
-	const [user, setUser] = useState({})
-	const [isAuth, setIsAuth] = useState(false)
-	const [theme, setTheme] = useState('')
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state.categories);
+  const { categories, isLoading, user, isAuth, theme } = state;
+  // const [categories, setCategories] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [user, setUser] = useState({});
+  // const [isAuth, setIsAuth] = useState(false);
+  // const [theme, setTheme] = useState("");
 
-	useEffect(() => {
-		const lastState = readLocalStorage('helmets')
-		if (!lastState) {
-			getProducts()
-				.then((data) => {
-					setCategories(data)
-					setIsLoading(true)
-				})
-				.catch(() => {
-					setIsLoading(false)
-				})
-			return
-		}
+  useEffect(() => {
+    const lastState = readLocalStorage("helmets");
+    if (!lastState) {
+      getProducts()
+        .then((data) => {
+          dispatch({ type: actionTypes.FETCH_SUCCESS, payload: data });
+        })
+        .catch(() => {
+          dispatch({ type: actionTypes.FETCH_ERROR });
+        });
+      return;
+    }
 
-		setCategories(lastState.categories)
-		setUser(lastState.user)
-		setIsAuth(lastState.isAuth)
-		setIsLoading(lastState.isLoading)
-		setTheme(lastState.theme)
-	}, [])
+    dispatch({ type: actionTypes.CATEGORIES, payload: lastState.categories });
+    dispatch({ type: actionTypes.USER, payload: lastState.user });
+    //setUser(lastState.user);
+    //setIsAuth(lastState.isAuth);
+    dispatch({ type: actionTypes.AUTH, payload: lastState.isAuth });
+    //setIsLoading(lastState.isLoading);
+    //setTheme(lastState.theme);
+  }, []);
 
-	useEffect(() => {
-		writeLocalStorage(
-			'helmets',
-			JSON.stringify({ categories, user, isLoading, isAuth, theme })
-		)
-	}, [categories, user, isLoading, isAuth, theme])
+  useEffect(() => {
+    writeLocalStorage(
+      "helmets",
+      JSON.stringify({ categories, user, isLoading, isAuth, theme })
+    );
+  }, [categories, user, isLoading, isAuth, theme]);
 
-	const login = (values) => {
-		setUser({
-			name: values.name,
-			lastName: values.lastName,
-			email: values.email,
-		})
-		setIsAuth(true)
-	}
+  const login = (values) => {
+    dispatch({
+      type: actionTypes.USER,
+      payload: {
+        name: values.name,
+        lastName: values.lastName,
+        email: values.email,
+      },
+    });
+    dispatch({ type: actionTypes.AUTH, payload: true });
+  };
 
-	const logout = () => {
-		setUser({})
-		setIsAuth(false)
-	}
+  const logout = () => {
+    //setUser({});
+    dispatch({
+      type: actionTypes.USER,
+      payload: {},
+    });
+    //setIsAuth(false);
+    dispatch({ type: actionTypes.AUTH, payload: false });
+  };
 
-	const getTheme = () => {
-		theme === 'dark' ? setTheme('light') : setTheme('dark')
-	}
+  const getTheme = () => {
+    theme === "dark"
+      ? dispatch({ type: actionTypes.THEME, payload: "light" })
+      : dispatch({ type: actionTypes.THEME, payload: "dark" });
+  };
 
-	const themeMode = theme === 'light' ? lightTheme : darkTheme
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
 
-	return (
-		<AuthContext.Provider
-			value={{
-				user: user,
-				isAuthenticated: isAuth,
-				login: login,
-				logout: logout,
-				theme: getTheme,
-			}}
-		>
-			<ThemeProvider theme={themeMode}>
-				<GlobalStyles />
-				<ProductContext.Provider
-					value={{
-						categories: categories,
-						isLoading: isLoading,
-					}}
-				>
-					<Switch>
-						<Route
-							path={routes.HOME}
-							exact
-							render={(routeProps) => <Home {...routeProps} />}
-						/>
-						<Route
-							path={`${routes.HELMETS}/:category`}
-							exact
-							render={(routeProps) => <Helmets {...routeProps} />}
-						/>
-					</Switch>
-					<Switch>
-						<Route
-							path={routes.LOGIN}
-							exact
-							render={(routeProps) => <Login {...routeProps} />}
-						/>
-					</Switch>
-				</ProductContext.Provider>
-			</ThemeProvider>
-		</AuthContext.Provider>
-	)
+  return (
+    <AuthContext.Provider
+      value={{
+        user: user,
+        isAuthenticated: isAuth,
+        login: login,
+        logout: logout,
+        theme: getTheme,
+      }}
+    >
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyles />
+        <ProductContext.Provider
+          value={{
+            categories: categories,
+            isLoading: isLoading,
+          }}
+        >
+          <Switch>
+            <Route
+              path={routes.HOME}
+              exact
+              render={(routeProps) => <Home {...routeProps} />}
+            />
+            <Route
+              path={`${routes.HELMETS}/:category`}
+              exact
+              render={(routeProps) => <Helmets {...routeProps} />}
+            />
+          </Switch>
+          <Switch>
+            <Route
+              path={routes.LOGIN}
+              exact
+              render={(routeProps) => <Login {...routeProps} />}
+            />
+          </Switch>
+        </ProductContext.Provider>
+      </ThemeProvider>
+    </AuthContext.Provider>
+  );
 }
 
-export default App
+export default App;
